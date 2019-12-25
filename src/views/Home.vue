d77f6d7bc4813a1a6cff4e4dc015afb6e36703ac
victorwang0526 <fengxuanwang@qq.com>
2019-12-25 08:54:28 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <div>
+      <img alt="Vue logo" src="../assets/logo.png">
+    </div>
+    <div>
+      <router-link :to="'/about'">
+        <ion-button>About</ion-button>
+      </router-link>
+    </div>
+    <div>
+      <router-link to="/about">
+        <van-button type="primary">Vant Button</van-button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+@Component({
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+})
+export default class Home extends Vue{
+
+}
+</script>
