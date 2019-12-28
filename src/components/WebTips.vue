ae37249137eac18334ca59871c8cc453f5341949
victor <fengxuanwang@qq.com>
2019-12-28 22:00:33 +0800

    img

--- src/components/WebTips.vue
+++ src/components/WebTips.vue
@@ -9,6 +9,8 @@
 import {Component, Vue} from "vue-property-decorator";
 import moment from 'moment'
 import axios from "axios";
+import { Plugins } from '@capacitor/core';
+const { Toast } = Plugins;
 
 @Component({
   name: 'web-tips'
@@ -25,8 +27,16 @@ export default class extends Vue {
 
     axios.get('http://47.110.45.52:8085/api/task/ip')
         .then((response: any) => {
-            (this.ip = response.data)
+          this.ip = response.data
+          Toast.show({
+            text: JSON.stringify(response.data)
+          });
         })
+    .catch(err => {
+        Toast.show({
+          text: JSON.stringify(err)
+        });
+    })
   }
 }
 </script>
